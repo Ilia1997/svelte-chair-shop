c810640266eef4e0b2bef4aa2359482114a8ed81
Ilia lunha <ilia97ap76@gmail.com>
2023-01-19 15:01:31 +0200

    change env export

--- src/routes/shoping-cart/+page.svelte
+++ src/routes/shoping-cart/+page.svelte
@@ -222,27 +222,30 @@
         </div>
       </div>
     </div>
-  {:else if products.length === 0 && browser}
-    <div
-      class="my-7 p-12 w-full max-w-lg mx-auto flex items-center justify-center bg-white shadow-base"
-      in:fade
-    >
-      <div>
-        <h2 class="text-2xl font-lato text-shop-off-blue text-center">
-          Cart is empty
-        </h2>
-        <img
-          src={EmptyCartImage}
-          alt="Empty Cart"
-          in:scale={{ delay: 400, duration: 300 }}
-          class="mt-4 w-80 h-80"
-        />
-        <a
-          href="/products"
-          class="block text-center w-full py-2.5 bg-shop-green mt-10 text-white font-bold font-lato hover:bg-shop-purple"
-          >Go Shopping</a
-        >
-      </div>
-    </div>
   {/if}
+
+  <div
+    class="my-7 p-12 w-full max-w-lg mx-auto  items-center justify-center bg-white shadow-base {products.length ===
+      0 && browser
+      ? 'flex'
+      : 'hidden'}"
+    in:fade
+  >
+    <div>
+      <h2 class="text-2xl font-lato text-shop-off-blue text-center">
+        Cart is empty
+      </h2>
+      <img
+        src={EmptyCartImage}
+        alt="Empty Cart"
+        in:scale={{ delay: 400, duration: 300 }}
+        class="mt-4 w-80 h-80"
+      />
+      <a
+        href="/products"
+        class="block text-center w-full py-2.5 bg-shop-green mt-10 text-white font-bold font-lato hover:bg-shop-purple"
+        >Go Shopping</a
+      >
+    </div>
+  </div>
 </div>
