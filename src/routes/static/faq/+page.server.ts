330e225afa6978360cf4b9168901398a365a4043
Max <skiff4ek@gmail.com>
2023-01-23 12:28:02 +0200

    Quastions Forms

--- src/routes/static/faq/+page.server.ts
+++ src/routes/static/faq/+page.server.ts
@@ -1,5 +1,32 @@
 import type { PageServerLoad } from "./$types";
 import client from "../../../sanityClient";
+import { AuthApiError } from "@supabase/gotrue-js";
+import { fail, type Actions } from "@sveltejs/kit";
+
+export const actions: Actions = {
+  question: async ({ request, locals }) => {
+    const body = Object.fromEntries(await request.formData());
+    const { data, error: err } = await locals.sb.from('QuestionForm').insert({ 
+      user_name: body.username,
+      user_email: body.email,
+      form_subject: body.subject,
+      form_body: body.message,
+    })
+    if (err) {
+      console.log(err)
+      if (err instanceof AuthApiError && err.status == 500) {
+        return fail(500, {
+          error: {
+            email: "",
+            password: "",
+            all: "Server error. Please try again later.",
+          },
+        });
+      } 
+    }
+    return { success: true };
+  },
+};
 
 export const load = (async () => {
   const queryPageInfo = `*[_type == "FAQ"] {
